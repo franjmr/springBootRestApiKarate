a68311bbfbb3834338c287496ca8ac32c2cd74c8
franjmr <pintao2x@gmail.com>
2021-01-14 15:04:45 +0100

    initial

---
+++ src/main/java/com/zara/procedures/ProceduresApplication.java
@@ -0,0 +1,13 @@
+package com.zara.procedures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProceduresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProceduresApplication.class, args);
+	}
+
+}
