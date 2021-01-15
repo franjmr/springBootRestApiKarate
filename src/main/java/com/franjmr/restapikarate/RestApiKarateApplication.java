7e315107cda60aea56afe7fff1c1d0c4d9440557
franjmr <pintao2x@gmail.com>
2021-01-15 08:06:07 +0100

    Rename SpringBootApplication main classs

--- src/main/java/com/franjmr/restapikarate/RestApiKarateApplication.java
+++ src/main/java/com/franjmr/restapikarate/RestApiKarateApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProceduresApplication {
+public class RestApiKarateApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProceduresApplication.class, args);
+		SpringApplication.run(RestApiKarateApplication.class, args);
 	}
 
 }
