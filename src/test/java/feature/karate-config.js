438be75f04621aa94b8762083c518c353a576b7c
franjmr <pintao2x@gmail.com>
2021-02-05 14:38:54 +0100

    Delete deprecated files

--- src/test/java/feature/karate-config.js
+++
@@ -1,6 +0,0 @@
-function karateconf() {
-  karate.configure('connectTimeout', 5000);
-  karate.configure('readTimeout', 5000);
-  var config = { baseURL: 'http://localhost:8080' };
-  return config;
-}
